73d43ed9431b5e097313b3ca3be38520cf5dc42a
valeraron <77055501+valeraron@users.noreply.github.com>
2021-09-06 02:55:50 +0300

    create project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Conteiner } from './App.styled';
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    this.repeatControl(data);
+  };
+
+  repeatControl = data => {
+    let nameArray = [];
+    nameArray = this.state.contacts.map(con => con.name);
+    if (!nameArray.includes(data.name)) {
+      let arrayCont = [];
+      arrayCont = [
+        ...this.state.contacts,
+        { id: uuidv4(), name: data.name, number: data.number },
+      ];
+      return this.setState({ ...this.state, contacts: arrayCont });
+    } else {
+      alert(`${data.name}вже є у телефонній книзі!!!`);
+    }
+  };
+
+  elementDelete = (arr, idContact) => {
+    let newArr = arr.filter(elem => elem.id !== idContact);
+    return newArr;
+  };
+
+  deleteContactFromContactList = idContact => {
+    let newArrAfterDel = this.elementDelete(this.state.contacts, idContact);
+    this.setState({
+      ...this.state,
+      contacts: [...newArrAfterDel],
+    });
+  };
+
+  setFilterToState = filterData => {
+    this.setState({ ...this.state, filter: `${filterData}` });
+  };
+
+  filterArr = fArr => {
+    let newArr = fArr.filter(cont =>
+      cont.name.toUpperCase().includes(this.state.filter),
+    );
+    return newArr;
+  };
+
+  render() {
+    return (
+      <Conteiner>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitData={this.formSubmitHandler} />
+        <h1>Contacts</h1>
+        <Filter setFilterToState={this.setFilterToState} />
+        <ContactList
+          contacts={this.filterArr(this.state.contacts)}
+          del={this.deleteContactFromContactList}
+        />
+      </Conteiner>
+    );
+  }
+}
+export default App;
---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+export const ConteinerForm = styled.div`
+  width: 300px;
+  padding: 20px;
+  display: block;
+  border: 1px solid #477405;
+`;
+
+export const Button = styled.button`
+  margin: 5px 10px;
+  background-color: #0a0131ab;
+  color: white;
+  border-radius: 7px;
+  border-color: #0a0131ab;
+  &:hover {
+    background-color: white;
+    color: #0a0131ab;
+    border-color: #0a0131ab;
+  }
+`;
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  max-width: 200px;
+`;
